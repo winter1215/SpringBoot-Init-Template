008f59a0f0a57f5c06cbac590d6cbb8708dceba0
winter <2436197699@qq.com>
2024-01-18 14:26:43 +0800

    feat: 新增两个单独的模板

---
+++ springboot-init-intercepter/src/main/java/com/winter/init/controller/PostThumbController.java
@@ -0,0 +1,53 @@
+package com.winter.init.controller;
+
+import com.winter.init.common.BaseResponse;
+import com.winter.init.common.ErrorCode;
+import com.winter.init.common.ResultUtils;
+import com.winter.init.config.exception.BusinessException;
+import com.winter.init.model.dto.postthumb.PostThumbAddRequest;
+import com.winter.init.model.entity.User;
+import com.winter.init.service.PostThumbService;
+import com.winter.init.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser();
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
---
+++ springboot-init-intercepter/src/main/java/com/winter/init/model/dto/postfavour/PostFavourQueryRequest.java
@@ -0,0 +1,27 @@
+package com.winter.init.model.dto.postfavour;
+
+import com.winter.init.common.PageRequest;
+import com.winter.init.model.dto.post.PostQueryRequest;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 帖子收藏查询请求
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PostFavourQueryRequest extends PageRequest implements Serializable {
+
+    /**
+     * 帖子查询请求
+     */
+    private PostQueryRequest postQueryRequest;
+
+    /**
+     * 用户 id
+     */
+    private Long userId;
+
+    private static final long serialVersionUID = 1L;
+}
