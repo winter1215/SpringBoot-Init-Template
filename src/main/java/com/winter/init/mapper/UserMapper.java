74e983f794d645f0e459cb4b7d4c904cc85cbf3e
winter <2436197699@qq.com>
2024-01-18 12:15:35 +0800

    refactor: 重构项目名

---
+++ src/main/java/com/winter/init/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.winter.init.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.winter.init.model.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
