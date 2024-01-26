c3fd0948aa7c1ad164318e788bf0065049d418ca
winter <2436197699@qq.com>
2024-01-26 16:21:47 +0800

    feat: 新增 chatGpt-sdk 接入

--- springboot-init-intercepter/src/test/java/com/winter/init/MainApplicationTests.java
+++ springboot-init-intercepter/src/test/java/com/winter/init/MainApplicationTests.java
@@ -1,6 +1,8 @@
 package com.winter.init;
 
 import javax.annotation.Resource;
+
+import com.plexpt.chatgpt.ChatGPT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,12 +13,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MainApplicationTests {
 
-//    @Resource
-//    private WxOpenConfig wxOpenConfig;
-//
-//    @Test
-//    void contextLoads() {
-//        System.out.println(wxOpenConfig);
-//    }
+    @Test
+    void contextLoads() {
+        ChatGPT chatGPT = ChatGPT.builder()
+                .apiKey("")
+                .apiHost("https://cfwus02.opapi.win/")
+                .build()
+                .init();
+        String ans = chatGPT.chat("鲁迅为什么暴打周树人");
+        System.out.println(ans);
+    }
 
 }
